3f37d133d9d9fbaf6441bbeacd83923c4044d762
Quaad <ajwood1998@hotmail.co.uk>
2025-02-09 23:04:19 +0000

    added new index to jobs and updated controller with index method to fetch featured and nonfeatured jobs to be displayed. Also updated components to accept job and tag props

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,19 +1,21 @@
+@props(['job'])
+
 <x-panel>
     <div>
         <x-employer-logo></x-employer-logo>
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full time - From $60,000</p>
+        <a href="#" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">{{ $job->title }}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->schedule }} - {{ $job->salary }}</p>
     </div>
 
     <div>
         <div>                        
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
+            @foreach ($job->tags as $tag)
+                <x-tag size="small">{{ $tag->name }}</x-tag>
+            @endforeach   
         </div>
 
     </div>
